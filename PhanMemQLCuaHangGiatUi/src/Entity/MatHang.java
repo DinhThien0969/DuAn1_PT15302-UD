6ece00bfc9f00e8c32faf5f2152a91a5406d18e2
= <=>
2020-11-20 09:04:30 +0700

    comment

--- PhanMemQLCuaHangGiatUi/src/Entity/MatHang.java
+++ PhanMemQLCuaHangGiatUi/src/Entity/MatHang.java
@@ -3,5 +3,58 @@ package Entity;
 
 
 public class MatHang {
-    
+    String MaMH ;
+    String TenMH;
+    int SoLuong ;
+    Boolean TrangThai;
+    String MaNV;
+     String MaDV; 
+
+    public String getMaMH() {
+        return MaMH;
+    }
+
+    public void setMaMH(String MaMH) {
+        this.MaMH = MaMH;
+    }
+
+    public String getTenMH() {
+        return TenMH;
+    }
+
+    public void setTenMH(String TenMH) {
+        this.TenMH = TenMH;
+    }
+
+    public int getSoLuong() {
+        return SoLuong;
+    }
+
+    public void setSoLuong(int SoLuong) {
+        this.SoLuong = SoLuong;
+    }
+
+    public Boolean getTrangThai() {
+        return TrangThai;
+    }
+
+    public void setTrangThai(Boolean TrangThai) {
+        this.TrangThai = TrangThai;
+    }
+
+    public String getMaNV() {
+        return MaNV;
+    }
+
+    public void setMaNV(String MaNV) {
+        this.MaNV = MaNV;
+    }
+
+    public String getMaDV() {
+        return MaDV;
+    }
+
+    public void setMaDV(String MaDV) {
+        this.MaDV = MaDV;
+    }
 }
